4f5339b2444efcdd8f0cccc841ed86753f54a62c
Olive <olivelliott48@gmail.com>
2022-07-06 21:27:03 -0400

    Add route to update posts title

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -65,4 +65,30 @@ router.post("/", (req, res) => {
     });
 });
 
+// Update a post's title
+router.put("/:id", (req, res) => {
+    Post.update(
+        {
+        title: req.body.title,
+        },
+        {
+        where: {
+            id: req.params.id,
+        },
+        }
+    )
+        .then((dbPostData) => {
+        if (!dbPostData) {
+            res.status(404).json({ message: "No post found with this id" });
+            return;
+        }
+        res.json(dbPostData);
+        })
+        
+        .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+        });
+});
+
 module.exports = router;
