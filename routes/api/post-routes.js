4687ddf4c11aab25ab5345995e9b960afaef8ea4
Olive <olivelliott48@gmail.com>
2022-07-07 15:56:06 -0400

    Refactor vote routes and model

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -7,7 +7,13 @@ router.get('/', (req, res) => {
     console.log('========================');
     Post.findAll({
         // Query config
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -26,30 +32,39 @@ router.get('/', (req, res) => {
 // GET one specific post
 router.get("/:id", (req, res) => {
     Post.findOne({
-        where: {
+      where: {
         id: req.params.id,
-        },
-        attributes: ["id", "post_url", "title", "created_at"],
-        include: [
-        {
-            model: User,
-            attributes: ["username"],
-        },
+      },
+      attributes: [
+        "id",
+        "post_url",
+        "title",
+        "created_at",
+        [
+          sequelize.literal(
+            "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"),"vote_count"
         ],
+      ],
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
     })
-    .then((dbPostData) => {
+      .then((dbPostData) => {
         if (!dbPostData) {
-            res.status(404).json({ message: "No post found with this id" });
-            return;
+          res.status(404).json({ message: "No post found with this id" });
+          return;
         }
 
         res.json(dbPostData);
-        })
+      })
 
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json(err);
-    });
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 // CREATE a post
@@ -67,35 +82,15 @@ router.post("/", (req, res) => {
     });
 });
 
+
 // PUT route for voting on a post
 router.put('/upvote', (req, res) => {
-    Vote.create({
-        user_id: req.body.user_id,
-        post_id: req.body.post_id,
-    })
-    .then(() => {
-  // then find the post we just voted on
-        return Post.findOne({
-            where: {
-                id: req.body.post_id
-            },
-            attributes: [
-                'id',
-                'post_url',
-                'title',
-                'created_at',
-                // use raw MySQL aggregate function query to get a count of how many votes the post has and return it under the name `vote_count`
-                [
-                    sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-                    'vote_count'
-                ]
-            ]
-        })
-        .then(dbPostData => res.json(dbPostData))
-        .catch(err => {
-            console.log(err);
-            res.status(400).json(err);
-        });
+    //  custom static method created in models/Post.js
+    Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
     });
 });
 
