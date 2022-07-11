f4beb017ec72dbfe404781454ff92e049ef7311b
Olive <olivelliott48@gmail.com>
2022-07-10 20:06:43 -0400

    Complete comment routes for Post and User models

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require("../../config/connection");
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Vote, Comment } = require('../../models');
 
 //  GET all posts
 router.get('/', (req, res) => {
@@ -16,6 +16,14 @@ router.get('/', (req, res) => {
         ],
         order: [['created_at', 'DESC']],
         include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
             {
                 model: User,
                 attributes: ['username']
@@ -47,8 +55,16 @@ router.get("/:id", (req, res) => {
       ],
       include: [
         {
-          model: User,
-          attributes: ["username"],
+            model: Comment,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+                model: User,
+                attributes: ['username']
+            }
+        },
+        {
+            model: User,
+            attributes: ["username"],
         },
       ],
     })
